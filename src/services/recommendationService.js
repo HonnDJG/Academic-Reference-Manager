4c61dab27467dcb86b90e85cc1bdcc85682aacf1
Daniel <danielevalberg@gmail.com>
2019-11-03 22:41:15 +0000

    added no recommendation message

--- src/services/recommendationService.js
+++ src/services/recommendationService.js
@@ -9,9 +9,10 @@ module.exports = (context) => {
                 const userLoanedPublications = await db.Loan.getLoansByUserId(u_id).select('publication -_id');
                 const userLoanedPublicationsIdArr = userLoanedPublications.map(x => x.publication);
                 const recommendations = await db.Review.getRecommendationExcludingBorrowed(userLoanedPublicationsIdArr);
+                if (!recommendations.length) { return "We have to specific recommendations for you! This is because we have not yet gotten any reviews from anybody."; }
                 return recommendations;
             } catch (e) {
-                console.log(e);
+                throwCreator.createThrow(e);
             }
         }
     }
